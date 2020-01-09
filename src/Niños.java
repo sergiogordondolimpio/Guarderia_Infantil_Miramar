26d55ea957fb21b8fd64e03a1bda630a8b0b38b9
sergiogordon <sergiogordon78@gmail.com>
2020-01-09 18:22:40 -0300

    enviando los archivos

---
+++ src/Niños.java
@@ -0,0 +1,16 @@
+import static java.lang.System.out;
+
+class Children {
+
+    String name;
+    String surname;
+    String birthDate;
+    int age;
+    String plan;
+
+    public void ver(){
+        System.out.println();
+    }
+
+
+}
